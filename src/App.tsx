ea29490463f8efdfcd386db0361efb41883c87be
thienhieu215 <thienhieu215@gmail.com>
2021-09-06 04:12:21 +0700

    instructions added & 404 page added

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,21 @@ import BottomNav from './components/bottomNav/BottomNav';
 
 function App() {
 
+  const bigScreen = useMediaQuery({ query: '(min-width: 769px)' })
   const mobileTablet = useMediaQuery({ query: '(max-width: 769px)' })
 
   return (
     <div className="App">
+
       <BrowserRouter>
+        {bigScreen &&
+          <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', padding: '15%' }}
+          >
+            <h1>Welcome to the site!
+              Please open the website on your mobile phone
+              or activate "mobile device emulation" in the inspect section of your browser.</h1>
+          </div>
+        }
         {mobileTablet &&
           <>
             <Route path='/' component={Routes} />
--- src/pages/404/PageNotFound.tsx
+++ src/pages/404/PageNotFound.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import img404 from './mascot_404.png'
 
 const PageNotFound = () => {
 
   return (
-    <div>Page Not Found</div>
+    <div >
+      <img style={{height: '160px', margin: '20px auto'}} src={img404}/>
+      <div>Xin lỗi, trang bạn tìm kiếm không tồn tại</div>
+    </div>
   )
 }
 
